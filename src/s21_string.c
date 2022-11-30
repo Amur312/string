d5ef4ac555087e2d19332c5d2a5057d44bd8c295
Amur312 <dzarasovamur@gmail.com>
2022-11-30 12:33:50 +0300

    30.11.2022

--- src/s21_string.c
+++ src/s21_string.c
@@ -1,1 +1,37 @@
 #include "s21_string.h"
+#include "string.h"
+int main() { 
+     // Исходный массив
+   unsigned char src[15]="123a56789";
+   // Переменная, в которую будет сохранен указатель
+   // на искомый символ.
+   char *sym;
+
+   // Вывод исходного массива
+   printf ("src old: %s\n",src);
+
+   // Поиск требуемого символа
+   sym = s21_memchr (src, 'a', 10);
+
+   // Если требуемый символ найден, то заменяем его
+   // на символ '!'
+   if (sym != NULL)
+      sym[0]='!';
+
+   // Вывод исходного массива
+   printf ("src new: %s\n",src);
+  
+
+   return 0;
+ }
+
+void *s21_memchr(const void *str, int c, size_t n) {
+
+    for(int i = 0; i < n; i++){
+        if(*((char *)str + i)== c){
+            return (char *) str + i;
+        }
+    }
+    return NULL;
+
+}
--- src/s21_string.h
+++ src/s21_string.h
@@ -2,4 +2,7 @@
 #ifndef _S21_STRING_H
 #define _S21_STRING_H
 #include <stdio.h>
+
+void *s21_memchr(const void *str, int c, size_t n);
+
 #endif // _S21_STRING_H
